b097d1aeaf81cf8c7d9fc253c80d737ea54ecaa2
Rodrigo Torres <rodrigo@familianascimento.org>
2012-09-03 08:27:11 -0400

    Mudança total. Primeira versão funcionando

---
+++ js/background.js
@@ -0,0 +1,77 @@
+﻿var active = {};
+var saved = {};
+
+function setIcon(tab){
+    var icon = active[tab.id]? 'on': 'off';
+
+    chrome.pageAction.setIcon({
+        'path': 'icons/' + icon + '.png',
+        'tabId': tab.id
+    }
+    )
+}
+
+function setTitle(tab){
+	var title = active[tab.id]? 'Deixa descer': 'Puxa o freio de mão';
+
+	chrome.pageAction.setTitle({
+		'title': title, 
+		'tabId': tab.id
+	}
+	)
+}
+
+function check(tab_id, data, tab){
+    if ( tab.url.match(/siafi2012/) ) {
+        chrome.pageAction.show(tab_id);
+
+        // Initialize with a valid value.
+        active[tab_id] = active[tab_id] || false;
+        setIcon(tab);
+    }
+}
+
+function clicked(tab){
+    var icon;
+
+    // Swap activation.
+    active[tab.id] = active[tab.id]? false : true;
+
+    // Set icon.
+    setIcon(tab);
+
+    // Set title.
+    setTitle(tab);
+
+    console.log('clicked: active[' + tab.id + ']= ' + active[tab.id]);
+}
+
+// Informs content_script if it Is authorized to reload.
+function autorizer(request, sender, sendResponse){
+    sendResponse({'data': active[sender.tab.id]});
+}
+
+// Save the text requested by content_script for later retrieval
+// (usually a command user has entered on the text field used
+// to reload the page)
+function saver(request, sender, sendResponse){
+    saved[sender.tab.id] = request.data;
+}
+function getter(request, sender, sendResponse){
+    sendResponse({'data': saved[sender.tab.id]});
+}
+
+function dispatcher(request, sender, sendResponse){
+    switch(request.cmd){
+        case 'autorize': autorizer(request, sender, sendResponse);
+                         break;
+        case 'saveText': saver(request, sender, sendResponse);
+                         break;
+        case 'getText' : getter(request, sender, sendResponse);
+                         break;
+    }
+}
+
+chrome.tabs.onUpdated.addListener(check);
+chrome.pageAction.onClicked.addListener(clicked);
+chrome.extension.onMessage.addListener(dispatcher);
---
+++ js/content_script.js
@@ -0,0 +1,36 @@
+﻿var timeout = 60000;
+
+// chrome.extension.sendMessage are assynchronous.
+
+function saveText(text){
+    chrome.extension.sendMessage({'cmd': 'saveText', 'data': text});
+}
+
+function getText(callback){
+    chrome.extension.sendMessage({'cmd' : 'getText'}, callback);
+}
+
+function reloudi(){
+	chrome.extension.sendMessage({'cmd': 'autorize'}, function(response){
+		var active = response.data;
+
+		if (active){
+			var rememberThis = $('#frmMenu :text').val();
+			saveText(rememberThis);
+
+			$('#frmMenu\\:acessoRapido').val('freeeeeia!');
+			$('#frmMenu :button').click();
+
+		}else{
+			window.setTimeout(reloudi, timeout);
+		}
+	});
+}
+
+// Recover saved field value.
+getText(function(response){ 
+	$('#frmMenu :text').val(response.data);
+});
+
+// Set timeout for the first reload.
+var timeoutId = window.setTimeout(reloudi, timeout);
